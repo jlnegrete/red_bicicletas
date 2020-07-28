618220f4ef9d89474b9a980bcb182809b8f41cdc
jlnegrete <negrete78@gmail.com>
2020-07-27 21:53:40 -0500

    pagine add crud

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -1,6 +1,21 @@
-//var map = L.map('main_map').setView([10.96854,-74.78132], 13);
-var map = L.map('main_map').setView([-34.6012424,-58.3861497], 13);
+var map = L.map('main_map').setView([10.96854,-74.78132], 13);
+//var map = L.map('main_map').setView([-34.6012424,-58.3861497], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+L.marker([10.984189,-74.791458]).addTo(map);
+//L.marker([-34.596932,-58.3808287]).addTo(map);
+//L.marker([-34.599564,-58.3778777]).addTo(map);
+
+$.ajax({
+	dataType: "json",
+	url: "api/bicicleta",
+	sucess: function(result){
+		console.log(result);
+		result.bicicleta.forEach(function(bici) {
+				L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
+		})
+	}
+})
